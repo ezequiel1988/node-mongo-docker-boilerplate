8f51f5be67811154d2aaef1afb973ef0e4044f8d
leo_depmerlo@hotmail.com <leo_depmerlo@hotmail.com>
2020-03-28 15:29:08 -0300

    fix en ruta get

--- api/routes/getRoute.js
+++ api/routes/getRoute.js
@@ -1,11 +1,8 @@
 module.exports = router => {
-   
-    router.get('/', async (req, res) => {
-
-        res.send({
-            message: 'Hello, thanks for using my apiRest, any questions or queries can be found at this email: ezequiel.leo.olivera@gail.com'
-        })
-    })
-  
-  };
-  
+  router.get("/", async (req, res) => {
+    res.send({
+      message:
+        "Hello, thanks for using my apiRest, any questions or queries can be found at this email: ezequiel.leo.olivera@gmail.com"
+    });
+  });
+};
